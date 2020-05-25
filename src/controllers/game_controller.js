80bd1148e9c2f5768b57ce237c910b39fed5ae76
chanchavac2022 <Jose.D.Chanchavac.22@dartmouth.edu>
2020-05-24 21:49:08 -0700

    finished dev site

---
+++ src/controllers/game_controller.js
@@ -0,0 +1,19 @@
+import Game from '../models/game_model';
+
+export const createGame = (fields) => {
+  const game = new Game(fields);
+  game.numPlayers = 1;
+  return game.save();
+};
+
+export const joinGame = (fields) => {
+  return Game.findOne({ sessionID: fields.sessionID })
+    .then((foundGame) => {
+      foundGame.playerIDs.push(fields.playerID);
+      foundGame.numPlayers += 1;
+      return foundGame.save();
+    })
+    .catch((error) => {
+      return { error };
+    });
+};
